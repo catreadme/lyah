265feb10b188575cef4a989de835d23e10c7ec54
Robin Elvedi <trivialic@posteo.net>
2016-03-27 14:55:01 +0200

    contiune writer

--- writer.hs
+++ writer.hs
@@ -1,4 +1,5 @@
 import Data.Monoid
+import Control.Monad.Writer
 
 isBigGang :: Int -> (Bool,String)
 isBigGang x = (x > 9,"Compared gang size to 9.")
@@ -17,3 +18,26 @@ addDrink :: Food -> (Food,Price)
 addDrink "beans" = ("milk", Sum 25)
 addDrink "jerky" = ("whiskey", Sum 99)
 addDrink _ = ("beer", Sum 30)
+
+-------------------------------------------
+
+logNum :: Int -> Writer [String] Int
+logNum x = writer (x, ["Got number: " ++ show x])
+
+multWithLog :: Writer [String] Int
+multWithLog = do
+  a <- logNum 3
+  b <- logNum 5
+  tell ["Gonna do some multiplication"]
+  return (a*b)
+
+-------------------------------------------
+
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a b
+  | b == 0 = do
+    tell ["Finished with " ++ show a]
+    return a
+  | otherwise = do
+    tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+    gcd' b (a `mod` b)
