f0274a870ce51f088de3d88e742aa25d3f2db8c8
Robin Elvedi <trivialic@posteo.net>
2016-03-27 20:40:24 +0200

    way too foldable

---
+++ foldable.hs
@@ -0,0 +1,21 @@
+import qualified Data.Foldable as F
+
+data Tree a = Empty | Node a (Tree a) (Tree a)
+  deriving (Show, Read, Eq)
+
+instance F.Foldable Tree where
+  foldMap f Empty = mempty
+  foldMap f (Node x l r) = F.foldMap f l `mappend`
+                           f x           `mappend`
+                           F.foldMap f r
+
+aTree :: Tree Int
+aTree = Node 5
+          (Node 3
+            (Node 1 Empty Empty)
+            (Node 6 Empty Empty)
+          )
+          (Node 9
+            (Node 8 Empty Empty)
+            (Node 10 Empty Empty)
+          )
