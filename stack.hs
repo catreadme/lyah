c207e3d07b02774b10ab523e7fc34900bea81ea0
Robin Elvedi <trivialic@posteo.net>
2016-03-27 15:25:51 +0200

    some stack

---
+++ stack.hs
@@ -0,0 +1,29 @@
+import Control.Monad.State
+
+type Stack = [Int]
+
+pop :: Stack -> (Int, Stack)
+pop (x:xs) = (x,xs)
+
+push :: Int -> Stack -> ((), Stack)
+push a xs = ((),a:xs)
+
+someStack :: Stack -> (Int, Stack)
+someStack stack = let
+  ((), newStack1) = push 3 stack
+  (a, newStack2) = pop newStack1
+  in pop newStack2
+
+-----------------------------------------
+
+pop' :: State Stack Int
+pop' = state $ \(x:xs) -> (x,xs)
+
+push' :: Int -> State Stack ()
+push' a = state $ \xs -> ((), a:xs)
+
+someStack' :: State Stack Int
+someStack' = do
+  push' 3
+  a <- pop'
+  pop'
