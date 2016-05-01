290adce796eb30b810d0705cae42cadd932b78cd
Robin Elvedi <trivialic@posteo.net>
2016-05-01 16:50:05 +0200

    start the hard part

---
+++ hard_part/functors_applicatives_monoids/1_functor.hs
@@ -0,0 +1,111 @@
+{-
+  In order to cover the whole section from the ground up, there will be
+  no Prelude to start with. Everything needed will be importet explicitly.
+-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+{-
+  Those are the things needed from Preludede.
+    - (+)
+      To be able to write some examples.
+    - Show
+      To be able to display custom data types
+    - Maybe(Just, Nothing)
+      To be able to implement "Functor" for the "Maybe" type.
+    - Either(Left, Right)
+      To be able to implement "Functor" for the "Either" type.
+-}
+import Prelude (
+               (+)
+               , Show
+               , Maybe(Just, Nothing)
+               , Either(Left, Right)
+               )
+
+{-
+  This is how "Functor" is defined in Prelude. The part with the "(<$)" operator
+  is omittet here for the sake of simplicity. From the type declaration it is
+  alredy kind of obvious what this thing is supposed to do.
+
+  "Take a function from "a" to "b", an "a" with some context, and return a "b" with
+  that same context"
+-}
+class Functor f where
+  fmap :: (a -> b) -> f a -> f b
+
+{-
+  Implementing the "Funtor" typeclass for the "List" type.
+
+  The implementation should look familiar. Here's how the well known "map"
+  is defined in Prelude.
+    map :: (a -> b) -> [a] -> [b]
+    map _ []     = []
+    map f (x:xs) = f x : map f xs
+-}
+instance Functor [] where
+  fmap _ [] = []
+  fmap f (x:xs) = f x : fmap f xs
+
+-- Examples:
+la = fmap (+1) [1] -- [2]
+lb = fmap (+1) [1..5] -- [2,3,4,5,6]
+
+{-
+  Implementing the "Funtor" typeclass for the "Maybe" type.
+-}
+instance Functor Maybe where
+  fmap _ Nothing = Nothing
+  fmap f (Just x) = Just (f x)
+
+-- Examples:
+ma = fmap (+1) (Just 1) -- Just 2
+mb = fmap (+1) Nothing -- Nothing
+
+{-
+  Implementing the "Funtor" typeclass for a "Tree" type.
+-}
+
+-- First the data type is declared.
+data Tree a = Node a (Tree a) (Tree a) | Empty
+  deriving (Show)
+
+-- The actual "Functor" implementation for a "Tree" type.
+instance Functor Tree where
+  fmap _ Empty = Empty
+  fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
+
+-- Examples:
+{-
+       5
+    3     8
+  1  4  6  10
+-}
+tt = Node 5 (Node 3
+              (Node 1 Empty Empty)
+              (Node 4 Empty Empty))
+            (Node 8
+              (Node 6 Empty Empty)
+              (Node 10 Empty Empty))
+
+ta = fmap (+1) tt
+ {-
+    Node 6
+      (Node 4
+        (Node 2 Empty Empty)
+        (Node 5 Empty Empty))
+      (Node 9
+        (Node 7 Empty Empty)
+        (Node 11 Empty Empty))
+ -}
+
+ {-
+   Implementing the "Funtor" typeclass for the "Either" type.
+ -}
+
+instance Functor (Either a) where
+  fmap f (Right x) = Right (f x)
+  fmap _ (Left x) = Left x
+
+-- Examples:
+ea = fmap (+1) (Right 5) -- Right 6
+eb = fmap (+1) (Left "Some error") -- Left "Some error"
---
+++ hard_part/readme.md
@@ -0,0 +1,10 @@
+# Hskell: The hard part
+This section contains what I personally experienced as being
+the "hard part" of haskell. It took multiple attempts to get this
+stuff right in my head.
+
+This section contains more in depth comments on each topic.
+
+It's mildly interesting to note that the alphabetical order of the directories
+in this section happens to also comply with the degree of difficulty of the topic
+they contain.
