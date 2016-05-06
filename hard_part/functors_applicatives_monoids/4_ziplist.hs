f52e8262724fa17ae63420a4b239cc97c45f635f
Robin Elvedi <trivialic@posteo.net>
2016-05-06 18:56:43 +0200

    zippers

--- hard_part/functors_applicatives_monoids/4_ziplist.hs
+++ hard_part/functors_applicatives_monoids/4_ziplist.hs
@@ -24,13 +24,19 @@ import Data.List
 
 {-
   Since the default Applicative behaviour of Lists is to produce what is
-  often called a cartesian product, one has to create a new type of List
-  in order to be able to defina a second, different applicative behaviour
-  of lists.
+  sometimes called a cartesian product, one has to create a new type of List
+  in order to be able to defina a second, different applicative behaviour.
+-}
+
+{-
+  A simple wrapper for a new type of List.
 -}
 newtype ZipList a = ZipList {getList :: [a]}
   deriving (Show)
 
+{-
+  Defining the Functor and Applicative classes.
+-}
 class Functor f where
   fmap :: (a -> b) -> f a -> f b
 
@@ -38,6 +44,15 @@ class (Functor f) => Applicative f where
   pure :: a -> f a
   (<*>) :: f (a -> b) -> f a -> f b
 
+{-
+  "(<$>)" is actually just "fmap" as an infix operator.
+-}
+(<$>) :: (Functor f) => (a -> b) -> f a -> f b
+f <$> x = fmap f x
+
+{-
+  Some preparation work.
+-}
 instance Functor [] where
   fmap _ [] = []
   fmap f (x:xs) = f x : fmap f xs
@@ -47,11 +62,28 @@ instance Functor ZipList where
   fmap f (ZipList (x:xs)) = ZipList (f x : fmap f xs)
 
 {-
-  "(<$>)" is actually just "fmap" as an infix operator.
+  The ZipList Applicative.
 -}
-(<$>) :: (Functor f) => (a -> b) -> f a -> f b
-f <$> x = fmap f x
-
 instance Applicative ZipList where
   pure x = ZipList (repeat x)
   ZipList fs <*> ZipList xs = ZipList (zipWith (\f x -> f x) fs xs)
+
+-- Examples:
+za = fmap (+1) (ZipList [1,2,3]) -- ZipList {getList = [2,3,4]}
+zb = (+1) <$> (ZipList [1,2,3]) -- ZipList {getList = [2,3,4]}
+
+zc = ZipList [(+10), (+100)] <*> ZipList [1,2] -- ZipList {getList = [11,102]}
+zd = (+) <$> ZipList [10,100] <*> ZipList [1,2] -- ZipList {getList = [11,102]}
+
+{-
+  Think of this like.
+    (a -> b -> c) -> (f a -> f b -> f c)
+-}
+liftA2 :: (Applicative f) => (a -> b -> c) -> f a -> f b -> f c
+liftA2 f a b = f <$> a <*> b
+
+-- Examples:
+la = liftA2 (+)
+lb = la (ZipList [1,2]) (ZipList [10,20]) -- ZipList {getList = [11,22]}
+
+lc = liftA2 (+) (ZipList [1,2]) (ZipList [10,20]) -- ZipList {getList = [11,22]}
